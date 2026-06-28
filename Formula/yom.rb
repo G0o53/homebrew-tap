b8970ae28663800c777e5965e9bddbf18c55e8fb
MatthewLizzard <161202393+MatthewLizzard@users.noreply.github.com>
2026-06-28 11:38:15 +1000

    hopefully this works?

--- Formula/yom.rb
+++ Formula/yom.rb
@@ -5,21 +5,9 @@ class Yom < Formula
   head "https://github.com/G0o53/yom.git", branch: "master"
   license "MIT"
 
+  depends_on "rust" => :build
+
   def install
-    user_home = ENV["HOME"] || File.expand_path("~")
-    rustup_bin = "#{user_home}/.cargo/bin"
-
-    ENV.prepend_path "PATH", rustup_bin
-
-    if which("cargo").nil?
-      odie <<~EOS
-        Rust is required to compile yom from source. 
-        Please install any version of Rust (we highly recommend rustup: https://rustup.rs) 
-        and try installing yom again.
-      EOS
-    end
-
     system "make", "homebrew", "PREFIX=#{prefix}"
   end
 end
-
