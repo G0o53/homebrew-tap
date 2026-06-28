55c9294a5004e450ed8b4cc92d629e561763e1b7
MatthewLizzard <161202393+MatthewLizzard@users.noreply.github.com>
2026-06-28 11:21:02 +1000

    trying to get tap working

--- Formula/yom.rb
+++ Formula/yom.rb
@@ -6,7 +6,10 @@ class Yom < Formula
   license "MIT"
 
   def install
-    if which("cargo").nil?
+    cargo_path = which("cargo") || 
+                 File.exist?("#{ENV["HOME"]}/.cargo/bin/cargo") ? "#{ENV["HOME"]}/.cargo/bin/cargo" : nil
+
+    if cargo_path.nil?
       odie <<~EOS
         Error: Rust is required to compile yom from source. 
         Please install any version of Rust (we highly recommend rustup: https://rustup.rs) 
@@ -14,6 +17,9 @@ class Yom < Formula
       EOS
     end
 
+    ENV.prepend_path "PATH", "#{ENV["HOME"]}/.cargo/bin"
+
     system "make", "homebrew", "PREFIX=#{prefix}"
   end
 end
+
