950039ff501a964f47ebb6c302bb8b0045e1a13a
MatthewLizzard <161202393+MatthewLizzard@users.noreply.github.com>
2026-06-29 16:49:40 +1000

    upgraded version to new gpl version

--- Formula/yom.rb
+++ Formula/yom.rb
@@ -1,7 +1,7 @@
 class Yom < Formula
   desc "lightweight, non-interactive micro-shell and systems glue"
   homepage "https://github.com/G0o53/yom"
-  url "https://github.com/G0o53/yom.git", tag: "1.2.4"
+  url "https://github.com/G0o53/yom.git", tag: "2.0.0"
   head "https://github.com/G0o53/yom.git", branch: "master"
   license "MIT"
 
